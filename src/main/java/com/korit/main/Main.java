705f92325f974519d5ac2c6088e7bf576b679084
doosan5464 <doosan5464@gmail.com>
2024-12-20 10:20:24 +0900

    ddd

--- src/main/java/com/korit/main/Main.java
+++ src/main/java/com/korit/main/Main.java
@@ -14,6 +14,8 @@ public class Main {
 //        userController.signIn(userService); // 계속 이렇게 새로 넣어줘야 한다 이런식이면
         userController.signUp(); // 컨트롤러에서 전역필드로 이미 Userservice타입의 필드를 만들었기때문에 이렇게 가능
         userController.signIn();
+        System.out.printf("5435345");
+        System.out.printf("5435345");
 
 //        userController.setUserService(userService2); // AllArgument, 그리고 @Setter가 있을시
 // 필요에 따라 둘을 선택해서 사용하면 된다
