4294292f513da22a00ca6694439af9a544369c72
doosan5464 <doosan5464@gmail.com>
2024-12-23 13:45:26 +0900

    24.12.23

--- src/main/java/com/korit/main/Main4.java
+++ src/main/java/com/korit/main/Main4.java
@@ -2,69 +2,22 @@ package com.korit.main;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main4 {
-
-    public static List<Integer> createListWithForLoop() {
-        List<Integer> tempList = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            tempList.add(i);
-        }
-        return tempList;
-    }
-
-    public static List<Integer> multiplyListWithForLoop(List<Integer> list) {
-        List<Integer> tempList = new ArrayList<>();
-        for (int num : list) {
-            tempList.add(num * 10);
-        }
-        return tempList;
-    }
-
-    // null값을 대체하기 위해 stream으로 1부터 10까지의 숫자를 만들어줌
-    public static List<Integer> createListWithStream() {
-        return IntStream.rangeClosed(1, 10) // stream 타입의 시작~끝까지의 숫자 지정
-                .boxed()    // 기본형에서 참조형으로
-                .collect(Collectors.toList());
-    }
-
-    public static List<Integer> multiplyListWithStream(List<Integer> list) {
-        return list.stream()
-                .map(num -> num * 10)
-                .collect(Collectors.toList());
-    }
-    /*
-    null - 주소할당 x
-
-     List<Integer> numbers = new ArrayList<>();
-     list.forEach(temp -> numbers.add(temp*10))
-
-     - 강사님 stream 버전
-     Stream<Integer> stream. stream도 타입개념
-     return numbers.stream().map(num->num*10).collect(Collectors.toList());
-
-    map 은 중간 가공. map도 결국 ()로 함수 호출. 그러므로 변수. 값이 들어가야 함 함수대신 람다?
-    기존 스트림을 기반으로 새로운 변환된 스트림을 생성한다
-     */
-
-
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        List<Integer> list2 = null;
-        List<Integer> list3 = null;
-
-        System.out.println("list: " + list);
-
-        list2 = createListWithForLoop();
-        list2 = multiplyListWithForLoop(list2);
-        System.out.println("list2: " + list2);
+        List<Integer> temp = List.of(1,2,3,4,5,6,7,8,9,10);
 
 
-        list3 = createListWithStream();
-        list3 = multiplyListWithStream(list3);
-        System.out.println("list3: " + list3);
+        List<Integer> temp4 = new ArrayList<>();
+        // List<Integer> temp4 = null
+        // temp4 = new ArrayList<>(); -> 컴파일 단계에서 밑에 forEach는 여기 대입으로 읽지 않고 위에 null로 인식.
+
+        // AtomicReference<Integer> ato = new AtomicReference<>(0); - 만약 전역변수를 람다식안에 쓰고 싶다면 타입을 이렇게 둬야하는데 나중에 학습
+
+        temp.forEach(number -> temp4.add(number));
+        // 함수조건문은 결국 지역변수. temp4에는 ArrayList가 들어와야 하는데 null로 초기화하고 밑에 대입하면 여기선 그렇게 읽지않음
+        System.out.println(temp4);
+
     }
 }
